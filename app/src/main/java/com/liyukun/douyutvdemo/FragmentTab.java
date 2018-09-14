a8d884a4fe706c7e0067d911460356a1e2201e6f
liyukun <416785731@qq.com>
2018-09-14 18:55:32 +0800

    tabbar初步搭建

---
+++ app/src/main/java/com/liyukun/douyutvdemo/FragmentTab.java
@@ -0,0 +1,45 @@
+package com.liyukun.douyutvdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentTab extends Fragment {
+    private static final String TAG = "FragmentTab";
+    private TextView mShowText;
+    private View mViewContent; // 缓存视图内容
+
+    @Nullable @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mViewContent == null) {
+            mViewContent = inflater.inflate(R.layout.fragment, null);
+        }
+
+        // 缓存View判断是否含有parent, 如果有需要从parent删除, 否则发生已有parent的错误.
+        ViewGroup parent = (ViewGroup) mViewContent.getParent();
+        if (parent != null) {
+            parent.removeView(mViewContent);
+        }
+
+//        ButterKnife.bind(this, mViewContent);
+        return mViewContent;
+    }
+
+    @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // 显示Fragment的Tag信息
+        mShowText = (TextView) mViewContent.findViewById(R.id.tab_show_text);
+        mShowText.setText(String.valueOf("Page: " + getTag()));
+        Log.e(TAG, "onViewCreated: "+getTag());
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+//        ButterKnife.unbind(this);
+    }
+}
--- app/src/main/java/com/liyukun/douyutvdemo/MainActivity.java
+++ app/src/main/java/com/liyukun/douyutvdemo/MainActivity.java
@@ -1,13 +1,58 @@
 package com.liyukun.douyutvdemo;
 
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentTabHost fragmentTabHost;
+    // 图片
+    private int imageButton[] = {
+            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_foreground
+    };
+    // 标题
+    private String texts[] = {"推荐","娱乐","关注","鱼吧","发现"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 实例化tabhost
+        fragmentTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        fragmentTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+//        fragmentTabHost.getTabWidget().setDividerDrawable(null); // 去掉分割线
+
+        for (int i = 0; i < texts.length; i++) {
+            // Tab按钮添加文字和图片
+            TabHost.TabSpec tabSpec = fragmentTabHost.newTabSpec(texts[i]).setIndicator(getImageView(i));
+            // 添加Fragment
+            fragmentTabHost.addTab(tabSpec, FragmentTab.class, null);
+            // 设置Tab按钮的背景(必须在addTab之后，由于需要子节点（底部菜单按钮）否则会出现空指针异常)
+            fragmentTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.color.white);
+        }
+    }
+
+    // 获得图片资源
+    private View getImageView(int index) {
+        //取得布局实例
+        View view = getLayoutInflater().inflate(R.layout.tab_content, null);
+//        View view = View.inflate(MainActivity.this, R.layout.tab_content, null);
+        //取得布局对象
+        ImageView imageView = (ImageView) view.findViewById(R.id.tab_bar_image);
+        TextView textView = (TextView) view.findViewById(R.id.tab_bar_text);
+        //设置图标
+        imageView.setImageResource(imageButton[index]);
+        //设置标题
+        textView.setText(texts[index]);
+        return view;
     }
 }
